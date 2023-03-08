3e1a2f48f732b3c800e7d027d3c501717d376e75
aliciahetrick <aliciahetrick@gmail.com>
2023-03-08 18:40:36 -0500

    Add initial movie data

---
+++ src/data.ts
@@ -0,0 +1,34 @@
+interface Movies  {
+  id: number;
+  title: string;
+  image: string;
+  genre?: 'action' | 'romance' | 'drama'
+}
+
+export const movies: Movies[] =[
+  {
+    id: 1,
+    title: "The Handmaiden",
+    image: "the-handmaiden.jpg",
+  },
+  {
+    id: 2,
+    title: "Thelma",
+    image: "thelma.jpg",
+  },
+  {
+    id: 3,
+    title: "Water Lilies",
+    image: "water-lilies.jpg",
+  },
+  {
+    id: 4,
+    title: "Happiest Season",
+    image: "water-lilies.jpg",
+  },
+  {
+    id: 5,
+    title: "Imagine Me & You",
+    image: "imagine-me-and-you.jpg",
+  },
+];
