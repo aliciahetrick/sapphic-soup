adac0104a4d0840442b89a983f2448d621e6f271
aliciahetrick <aliciahetrick@gmail.com>
2023-03-10 17:31:09 -0500

    Add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { Routes, Route } from 'react-router-dom'
 
 import AllMovies from './components/AllMovies'
 import Home from './components/Home'
+import Navbar from './components/Navbar'
 import RandomMovie from './components/RandomMovie'
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/allmovies" element={<AllMovies />}></Route>
