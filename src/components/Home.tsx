3558ed92be18ba23ee2e8570105d177e53b81ae4
aliciahetrick <aliciahetrick@gmail.com>
2023-03-11 12:52:32 -0500

    Update home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,14 +5,9 @@ import styled from 'styled-components'
 function Home() {
   return (
     <HomeWrapper>
-      <Link to="/allMovies" style={{ textDecoration: 'none' }}>
-        <HomeImage src="/images/thelma.jpg" />
-        <HomeTitle>Browse</HomeTitle>
-      </Link>
-      <Link to="/randomMovie" style={{ textDecoration: 'none' }}>
-        <HomeImage src="/images/the-handmaiden.jpg" />
-        <HomeTitle>Discover</HomeTitle>
-      </Link>
+      <HomeTitle>Sapphic Soup</HomeTitle>
+      <HomeImage src="/images/soup.png" />
+      <HomeTitle>LGBT movie recommendations that warm your soul.</HomeTitle>
     </HomeWrapper>
   )
 }
@@ -21,19 +16,21 @@ export default Home
 
 export const HomeWrapper = styled.div`
   display: flex;
-  gap: 5em;
+  flex-direction: column;
+  /* gap: 5em; */
   border-radius: 1em;
   margin-top: 15em;
   margin-bottom: 4em;
   justify-content: center;
+  align-items: center;
   border-radius: 1em;
 `
 
 export const HomeImage = styled.img`
   object-fit: cover;
-  width: 450px;
-  height: 350px;
-  border-radius: 1.5em;
+  width: 300px;
+  height: auto;
+  /* border-radius: 1.5em; */
 `
 
 export const HomeTitle = styled.h2`
