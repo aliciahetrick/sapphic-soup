e323ea02624d1f868ef2b227a6d5024b94c7c4ec
aliciahetrick <aliciahetrick@gmail.com>
2023-03-11 16:59:13 -0500

    Add all filter buttons

--- src/components/AllMovies.tsx
+++ src/components/AllMovies.tsx
@@ -8,17 +8,37 @@ function AllMovies() {
   const [selectedMovies, setSelectedMovies] = useState(movies)
 
   function filterMovies(genre: any) {
-    let filteredMovies = []
-    for (let i = 0; i < movies.length; i++) {
-      if (movies[i].genres?.includes(genre)) {
-        filteredMovies.push(movies[i])
+    if (genre === 'all') {
+      setSelectedMovies(movies)
+    } else {
+      let filteredMovies = []
+      for (let i = 0; i < movies.length; i++) {
+        if (movies[i].genres?.includes(genre)) {
+          filteredMovies.push(movies[i])
+        }
       }
+      setSelectedMovies(filteredMovies)
     }
-    setSelectedMovies(filteredMovies)
   }
   return (
     <Wrapper>
-      <div onClick={() => filterMovies('action')}>Action</div>
+      <FilterMoviesWrapper>
+        <FilteredMovie onClick={() => filterMovies('all')}>All</FilteredMovie>
+        <Divider>|</Divider>
+        <FilteredMovie onClick={() => filterMovies('action')}>Action</FilteredMovie>
+        <Divider>|</Divider>
+        <FilteredMovie onClick={() => filterMovies('comedy')}>Comedy</FilteredMovie>
+        <Divider>|</Divider>
+        <FilteredMovie onClick={() => filterMovies('drama')}>Drama</FilteredMovie>
+        <Divider>|</Divider>
+        <FilteredMovie onClick={() => filterMovies('horror')}>Horror</FilteredMovie>
+        <Divider>|</Divider>
+        <FilteredMovie onClick={() => filterMovies('romance')}>Romance</FilteredMovie>
+        <Divider>|</Divider>
+        <FilteredMovie onClick={() => filterMovies('sci-fi')}>Sci-fi</FilteredMovie>
+        <Divider>|</Divider>
+        <FilteredMovie onClick={() => filterMovies('thriller')}>Thriller</FilteredMovie>
+      </FilterMoviesWrapper>
       <AllMoviesWrapper>
         <MoviesList>
           {selectedMovies.map((movie) => {
@@ -37,6 +57,28 @@ export const Wrapper = styled.main`
   margin-top: 10em;
 `
 
+export const FilterMoviesWrapper = styled.main`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 1em;
+`
+
+export const FilteredMovie = styled.div`
+  color: #961e42;
+  font-family: Raleway;
+  font-weight: 600;
+  text-transform: uppercase;
+  cursor: pointer;
+`
+
+export const Divider = styled.div`
+  color: #961e42;
+  font-family: Raleway;
+  font-weight: 600;
+  text-transform: uppercase;
+`
+
 export const AllMoviesWrapper = styled.main`
   display: flex;
   flex-direction: row;
@@ -49,5 +91,5 @@ export const MoviesList = styled.section`
   flex-wrap: wrap;
   gap: 5em;
   margin-bottom: 8em;
-  margin-top: 10em;
+  margin-top: 3em;
 `
