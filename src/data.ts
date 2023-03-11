4d4fa56fc76861f68e454c627fc19a50cb281efe
aliciahetrick <aliciahetrick@gmail.com>
2023-03-11 09:01:11 -0500

    Seed more movies

--- src/data.ts
+++ src/data.ts
@@ -5,61 +5,157 @@ export const movies: Movies[] =[
     id: 1,
     title: "The Handmaiden",
     image: "the-handmaiden.jpg",
-    synopsis: "The handmaiden is set in Korea during the 1930s, under Japanese colonial rule, and tells the story of a young Korean woman named Sookee (Kim Tae-ri) who is hired as a handmaiden to a wealthy Japanese heiress named Lady Hideko (Kim Min-hee). Sookee's job is to assist Lady Hideko with her daily activities, while secretly plotting with a con man, Count Fujiwara (Ha Jung-woo), to steal her fortune."
+    synopsis: "A woman is hired as a handmaiden to a Japanese heiress, but secretly she is involved in a plot to defraud her."
   },
   {
     id: 2,
     title: "Thelma",
     image: "thelma.jpg",
-    synopsis: "Thelma is a quiet and introverted girl who struggles to make friends, but she begins to develop a relationship with another female student named Anja. As Thelma's feelings for Anja deepen, she starts experiencing strange and unexplainable seizures that seem to be connected to her emotions."
+    synopsis: "A confused religious girl tries to deny her feelings for a female friend who's in love with her. This causes her suppressed subconsciously-controlled psychokinetic powers to reemerge with devastating results."
   },
   {
     id: 3,
     title: "Water Lilies",
     image: "water-lilies.jpg",
-    synopsis: "The film centers on 15-year-old Marie, who is part of a synchronized swimming team. She develops a crush on Floriane, the team's captain, who is confident, sexy, and popular. However, Floriane seems to have eyes only for the boys on the swim team, and Marie finds herself struggling with her feelings for her."
+    synopsis: "After meeting at a local pool over their summer break, a love triangle forms between three adolescent girls, which proves difficult to sustain as they each desire the love of another."
   },
   {
     id: 4,
     title: "Happiest Season",
     image: "happiest-season.jpg",
-    synopsis: "Abby eagerly accepts the invitation to spend the holidays with Harper's family, but when they arrive at Harper's parents' house, Abby learns that Harper has not yet come out to her conservative family as gay. As a result, Abby has to pretend to be Harper's roommate and hide her true identity."
+    synopsis: "A holiday romantic comedy that captures the range of emotions tied to wanting your family's acceptance, being true to yourself, and trying not to ruin Christmas."
   },
   {
     id: 5,
     title: "Imagine Me & You",
     image: "imagine-me-and-you.jpg",
-    synopsis: "Rachel is about to marry her longtime boyfriend, Hector, when she meets Luce, who is the florist for her wedding. Rachel finds herself inexplicably drawn to Luce and begins to question her own sexuality. As she struggles with her newfound feelings, she has to navigate the complexities of her marriage, her family, and her own identity."
+    synopsis: "A newlywed bride becomes infatuated with another woman who questions her sexual orientation, promoting a stir among the bride's family and friends."
   },
   {
     id: 6,
     title: "Jack & Diane",
     image: "jack-and-diane.jpg",
-    synopsis: "Jack is a tough and rebellious girl from the Midwest who meets Diane, a soft-spoken and artistic girl from out of town. The two girls quickly develop a strong connection and begin to explore their budding romance. However, their relationship is complicated by Jack's fear of commitment and Diane's impending departure from the city."
+    synopsis: "Diane's feelings for Jack -- the girl she met over the summer -- begin to manifest themselves in terrifying ways when she learns that her friend will soon be moving away."
   },
   {
     id: 7,
     title: "The Half of It",
     image: "the-half-of-it.jpg",
-    synopsis: "One day, a shy jock named Paul Munsky hires Ellie to write a love letter to a girl named Aster Flores, who Ellie happens to have a crush on. As Ellie and Paul work together to win Aster's heart, they develop a close friendship, and Ellie realizes that she may have feelings for Paul as well."
+    synopsis: "When smart but cash-strapped teen Ellie Chu agrees to write a love letter for a jock, she doesn't expect to become his friend - or fall for his crush."
   },
   {
     id: 8,
     title: "D.E.B.S.",
     image: "debs.jpg",
-    synopsis: "The DEBS, which stands for Discipline, Energy, Beauty and Strength, are a top-secret organization that trains women to become highly skilled operatives. When one of their missions goes awry, the DEBS find themselves in the middle of a conspiracy involving a notorious villain named Lucy Diamond."
+    synopsis: "Plaid-skirted schoolgirls are groomed by a secret government agency to become the newest members of the elite national-defense group, D.E.B.S."
   },
   {
     id: 9,
     title: "What Keeps You Alive",
     image: "what-keeps-you-alive.jpg",
-    synopsis: "The movie follows a couple, Jules and Jackie, who retreat to a remote cabin in the woods for their one-year anniversary. However, as their vacation progresses, Jules discovers that Jackie is not who she appears to be and that their relationship is based on lies and deception."
+    synopsis: "Majestic mountains, a still lake and venomous betrayals engulf a female married couple attempting to celebrate their one-year anniversary."
   },
-  
   {
     id: 9,
     title: "But I'm a Cheerleader",
     image: "but-im-a-cheerleader.jpg",
-    synopsis: "The movie follows the story of Megan Bloomfield, a high school cheerleader who is sent to a residential conversion therapy camp when her parents suspect she is a lesbian."
+    synopsis: "A naive teenager is sent to rehab camp when her straitlaced parents and friends suspect her of being a lesbian."
   },
+  {
+    id: 10,
+    title: "The Kids Are All Right",
+    image: "the-kids-are-all-right.jpg",
+    synopsis: "Two children conceived by artificial insemination bring their biological father into their non-traditional family life."
+  },
+  {
+    id: 11,
+    title: "Girltrash: All Night Long",
+    image: "girltrash.jpg",
+    synopsis: "The story of five girls and one epic night. The girls will find love, lust, girl-fights, rock and roll, and a whole lot of stoned sorority girls."
+  },
+  {
+    id: 12,
+    title: "Disobedience",
+    image: "Disobedience.jpg",
+    synopsis: "A woman returns to her Orthodox Jewish community that shunned her for her attraction to a female childhood friend. Once back, their passions reignite as they explore the boundaries of faith and sexuality."
+  },
+  {
+    id: 13,
+    title: "Carol",
+    image: "carol.jpg",
+    synopsis: "An aspiring photographer develops an intimate relationship with an older woman in 1950s New York."
+  },
+  {
+    id: 14,
+    title: "Portrait of a Lady on Fire",
+    image: "portrait-of-a-lady-on-fire.jpg",
+    synopsis: "On an isolated island in Brittany at the end of the eighteenth century, a female painter is obliged to paint a wedding portrait of a young woman."
+  },
+  {
+    id: 15,
+    title: "Duck Butter",
+    image: "duck-butter.jpg",
+    synopsis: "Two women, who are dissatisfied with the dishonesty they see in dating and relationships, decide to make a pact to spend 24 hours together hoping to find a new way to create intimacy."
+  },
+  {
+    id: 16,
+    title: "Pariah",
+    image: "pariah.jpg",
+    synopsis: "A Brooklyn teenager juggles conflicting identities and risks friendship, heartbreak and family in a desperate search for sexual expression."
+  },
+  {
+    id: 17,
+    title: "Heavenly Creatures",
+    image: "heavenly-creatures.jpg",
+    synopsis: "Two teenage girls share a unique bond; their parents, concerned that the friendship is too intense, separate them, and the girls take revenge."
+  },
+  {
+    id: 18,
+    title: "Lost and Delirious",
+    image: "lost-and-delirious.jpg",
+    synopsis: "After starting at an upmarket boarding school, a teenage girl forms close friendships with her two older roommates. However, when she discovers that her new friends are lovers she finds herself caught in a complicated situation."
+  },
+  {
+    id: 19,
+    title: "Rafiki",
+    image: "rafiki.jpg",
+    synopsis: "'Good Kenyan girls become good Kenyan wives,' but Kena and Ziki long for something more. When love blossoms between them, the two girls will be forced to choose between happiness and safety."
+  },
+  {
+    id: 20,
+    title: "Crush",
+    image: "crush.jpg",
+    synopsis: "An aspiring young artist joins her high school track team and later discovers what real love feels like when she finds herself falling for an unexpected teammate."
+  },
+  {
+    id: 21,
+    title: "Everything Everywhere All at Once",
+    image: "everything-everywhere.jpg",
+    synopsis: "A middle-aged Chinese immigrant is swept up into an insane adventure in which she alone can save existence by exploring other universes and connecting with the lives she could have led."
+  },
+  {
+    id: 22,
+    title: "Certain Women",
+    image: "certain-women.jpg",
+    synopsis: "The lives of three women intersect in small-town America, where each is imperfectly blazing a trail."
+  },
+  {
+    id: 23,
+    title: "Kissing Jessica Stein",
+    image: "kissing-jessica-stein.jpg",
+    synopsis: "A woman searching for the perfect man instead discovers the perfect woman."
+  },
+  {
+    id: 24,
+    title: "The Hours",
+    image: "the-hours.jpg",
+    synopsis: "The story of how the novel 'Mrs. Dalloway' affects three generations of women, all of whom, in one way or another, have had to deal with suicide in their lives."
+  },
+  {
+    id: 25,
+    title: "Booksmart",
+    image: "booksmart.jpg",
+    synopsis: "On the eve of their high-school graduation, two academic superstars and best friends realize they should have worked less and played more. Determined not to fall short of their peers, the girls try to cram four years of fun into one night."
+  },
+
 ];
