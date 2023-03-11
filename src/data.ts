f6e1ca79d01f43a0cbd23dc7e2db814017bc2ee7
aliciahetrick <aliciahetrick@gmail.com>
2023-03-10 21:15:10 -0500

    Seed more movies

--- src/data.ts
+++ src/data.ts
@@ -31,4 +31,35 @@ export const movies: Movies[] =[
     image: "imagine-me-and-you.jpg",
     synopsis: "Rachel is about to marry her longtime boyfriend, Hector, when she meets Luce, who is the florist for her wedding. Rachel finds herself inexplicably drawn to Luce and begins to question her own sexuality. As she struggles with her newfound feelings, she has to navigate the complexities of her marriage, her family, and her own identity."
   },
+  {
+    id: 6,
+    title: "Jack & Diane",
+    image: "jack-and-diane.jpg",
+    synopsis: "Jack is a tough and rebellious girl from the Midwest who meets Diane, a soft-spoken and artistic girl from out of town. The two girls quickly develop a strong connection and begin to explore their budding romance. However, their relationship is complicated by Jack's fear of commitment and Diane's impending departure from the city."
+  },
+  {
+    id: 7,
+    title: "The Half of It",
+    image: "the-half-of-it.jpg",
+    synopsis: "One day, a shy jock named Paul Munsky hires Ellie to write a love letter to a girl named Aster Flores, who Ellie happens to have a crush on. As Ellie and Paul work together to win Aster's heart, they develop a close friendship, and Ellie realizes that she may have feelings for Paul as well."
+  },
+  {
+    id: 8,
+    title: "D.E.B.S.",
+    image: "debs.jpg",
+    synopsis: "The DEBS, which stands for Discipline, Energy, Beauty and Strength, are a top-secret organization that trains women to become highly skilled operatives. When one of their missions goes awry, the DEBS find themselves in the middle of a conspiracy involving a notorious villain named Lucy Diamond."
+  },
+  {
+    id: 9,
+    title: "What Keeps You Alive",
+    image: "what-keeps-you-alive.jpg",
+    synopsis: "The movie follows a couple, Jules and Jackie, who retreat to a remote cabin in the woods for their one-year anniversary. However, as their vacation progresses, Jules discovers that Jackie is not who she appears to be and that their relationship is based on lies and deception."
+  },
+  
+  {
+    id: 9,
+    title: "But I'm a Cheerleader",
+    image: "but-im-a-cheerleader.jpg",
+    synopsis: "The movie follows the story of Megan Bloomfield, a high school cheerleader who is sent to a residential conversion therapy camp when her parents suspect she is a lesbian."
+  },
 ];
