2ad08f8ba82a2bc149da761019e2dc3cf285cfd3
aliciahetrick <aliciahetrick@gmail.com>
2023-03-11 16:33:55 -0500

    Filter by action movies

--- src/components/AllMovies.tsx
+++ src/components/AllMovies.tsx
@@ -1,22 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { movies } from '../data'
 import { Card } from './Card'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
 function AllMovies() {
+  const [selectedMovies, setSelectedMovies] = useState(movies)
+  function actionButton() {
+    let actionMovies = []
+    for (let i = 0; i < movies.length; i++) {
+      if (movies[i].genres?.includes('action')) {
+        actionMovies.push(movies[i])
+      }
+    }
+    setSelectedMovies(actionMovies)
+  }
   return (
-    <AllMoviesWrapper>
-      <MoviesList>
-        {movies.map((movie) => {
-          return <Card key={movie.id} movies={movie} />
-        })}
-      </MoviesList>
-    </AllMoviesWrapper>
+    <Wrapper>
+      <div onClick={() => actionButton()}>Action</div>
+      <AllMoviesWrapper>
+        <MoviesList>
+          {selectedMovies.map((movie) => {
+            return <Card key={movie.id} movies={movie} />
+          })}
+        </MoviesList>
+      </AllMoviesWrapper>
+    </Wrapper>
   )
 }
 
 export default AllMovies
+
+export const Wrapper = styled.main`
+  margin-bottom: 8em;
+  margin-top: 10em;
+`
 
 export const AllMoviesWrapper = styled.main`
   display: flex;
