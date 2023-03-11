7c8820b875066ed317a272036d8457495266aa96
aliciahetrick <aliciahetrick@gmail.com>
2023-03-10 20:25:00 -0500

    Conditionally render side of movie card on click

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Movies from '../interfaces/Movies'
 
 interface Props {
@@ -6,10 +6,27 @@ interface Props {
 }
 
 export function Card({ movies }: Props) {
+  const [toggle, setToggle] = useState(false)
+
+  function toggleFunc() {
+    console.log('toggle', toggle)
+    setToggle(!toggle)
+  }
+
   return (
     <div>
-      <img src={`./images/${movies.image}`} alt="card" className="card-image" />
-      <div className="card-title">{movies.title}</div>
+      {toggle && (
+        <div onClick={() => toggleFunc()}>
+          <img className="card-image"></img>
+          <div className="card-title">{movies.title}</div>
+        </div>
+      )}
+      {!toggle && (
+        <div onClick={() => toggleFunc()}>
+          <img src={`./images/${movies.image}`} alt="card" className="card-image" />
+          <div className="card-title">{movies.title}</div>
+        </div>
+      )}
     </div>
   )
 }
